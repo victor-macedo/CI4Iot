2f2f1c05401f48b6ff55a38ea3d81d354a75c1cf
Victor Macedo <victormacedo@pop-os.localdomain>
2023-05-04 12:36:40 +0100

    Plot

--- lab3.py
+++ lab3.py
@@ -2,5 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('../CI4Iot/Lab3_DataSets/DCOILBRENTEUv2.csv')
-plt.plot(df["DCOILBRENTEU"])
+def remove_out():
+    
+
+df = pd.read_csv('../CI4Iot/Lab3_DataSets/EURUSD_Daily_Ask_2018.12.31_2019.10.05v2.csv')
+df["DATE"] = pd.to_datetime(df["Time (UTC)"])
+#plt.plot(df)
+plt.plot(df["DATE"],df["High"])
+plt.show()
