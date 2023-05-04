28f0413433809d3ee7be6d96f38c74097cde32bf
Victor Macedo <victormacedo@pop-os.localdomain>
2023-05-04 16:50:31 +0100

    Funcao remove outliers funcionando

--- lab3.py
+++ lab3.py
@@ -7,12 +7,14 @@ def remove_out(df,coluna,k):
     v = 0
     for i in range (0, len(df.index)):      
         u += df[coluna][i]     #Calculo da média
+    u = u/len(df.index)
     for i in range (0, len(df.index)):
         v += (df[coluna][i]-u)**2 #Calculo da somatoria da variancia
-    v = v/len(df.index)             #Calculo final variancia
+    v = np.sqrt(v/len(df.index))             #Calculo final variancia
     for i in range(0, len(df.index)):
-        if abs(df[coluna][i]) > k*v:
-            df.drop([i])
+        if abs(df[coluna][i]-u) > k*v:
+            df = df.drop(df.index[i])
+    return df
 
 
     
@@ -20,6 +22,6 @@ def remove_out(df,coluna,k):
 df = pd.read_csv('../CI4Iot/Lab3_DataSets/EURUSD_Daily_Ask_2018.12.31_2019.10.05v2.csv')
 df["DATE"] = pd.to_datetime(df["Time (UTC)"])
 #plt.plot(df)
-remove_out(df,"High",2)
+df = remove_out(df,"High",0.6)
 plt.plot(df["DATE"],df["High"])
 plt.show()
