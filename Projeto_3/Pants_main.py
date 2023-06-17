79e57d3a99caf42aac4c7dc3dc278a3f53503dd6
Victor Macedo <victormacedo@pop-os.localdomain>
2023-06-17 12:14:15 +0100

    Teste com formigas individuais

--- Projeto_3/Pants_main.py
+++ Projeto_3/Pants_main.py
@@ -11,14 +11,41 @@ def euclidean(a, b):
 world = pants.World(nodes, euclidean)
 ids = world.nodes
 
+ants = []
+ant = pants.Ant(alpha=0.5, beta=2.25)
+ant.initialize(world, start=world.nodes[0])
+for i in range(100):
+   ants.append(ant.clone())
+
+for a in ants:
+  for i in range (100):
+    if ant.can_move() == True:
+      ant.move()
+
+B_ant = ant
+for a in ants:
+   if (a.distance < B_ant.distance) & (a.distance != 0):
+      B_ant = a
+print("Resultados formiga \n")
+
+print(B_ant.distance)
+print(B_ant.tour)
+
 solver = pants.Solver()
-solution = solver.solve(world)
+#solver.create_colony(world)
+solver.find_solutions(ants)
+solver.global_update(ants)
+
+
+solution = solver.aco(ants)
 # or
 solutions = solver.solutions(world)
 
+print("Resultados solver \n")
+
 print(solution.distance, "\n")
 print(solution.tour,"\n")    # Nodes visited in order
-print(solution.path,"\n")    # Edges taken in order
+#print(solution.path,"\n")    # Edges taken in order
 # or
 best = float("inf")
 for solution in solutions:
